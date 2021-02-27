2d92f4e0299d015600734b8906898e61b90799ba
TreeKR <johanbag290@gmail.com>
2021-02-27 12:58:57 +0900

    Commit

---
+++ src/main/java/xyz/namutree0345/powerer/Powerer.java
@@ -0,0 +1,17 @@
+package xyz.namutree0345.powerer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Powerer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
