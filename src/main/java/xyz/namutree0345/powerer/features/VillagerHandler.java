d7b1fd346c8342a33f62e692bf17e00276d7e07e
TreeKR <johanbag290@gmail.com>
2021-02-27 15:29:04 +0900

    1.16.5

--- src/main/java/xyz/namutree0345/powerer/features/VillagerHandler.java
+++ src/main/java/xyz/namutree0345/powerer/features/VillagerHandler.java
@@ -1,7 +1,5 @@
 package xyz.namutree0345.powerer.features;
 
-import net.kyori.adventure.text.Component;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -14,10 +12,6 @@ import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.awt.*;
-import java.awt.print.Book;
-import java.util.ArrayList;
 
 public class VillagerHandler implements Listener {
 
@@ -61,9 +55,7 @@ public class VillagerHandler implements Listener {
                             BookMeta bm = (BookMeta) book.getItemMeta();
                             bm.setAuthor("하늘섬 이장");
                             bm.setTitle("하늘섬에서 살아남기");
-                            bm.addPages(Component.text("일단 조약돌 생성기를 만드세요.\n그 다음에 여러 광물을 캐세요.\n광물은 무제한으로 계속 생성됩니다.\n광물을 캐서 엔드 요새에 가세요."),
-                                    Component.text("엔드 주민에게 바치라는것을 바치세요.\n상점에서 사야되는것도 있습니다.\n그다음에 엔드 주민에게 받은 엔드 진주와\n블레이즈 막대를 조합해 엔더 월드에 가세요.\n엔더드래곤을 잡은 사람은"),
-                                    Component.text("매우 좋은 일이 있을거에요!\n그러면 희망을 빕니다."));
+                            bm.addPage("일단 조약돌 생성기를 만드세요.\n그 다음에 여러 광물을 캐세요.\n광물은 무제한으로 계속 생성됩니다.\n광물을 캐서 엔드 요새에 가세요.", "엔드 주민에게 바치라는것을 바치세요.\n상점에서 사야되는것도 있습니다.\n그다음에 엔드 주민에게 받은 엔드 진주와\n블레이즈 막대를 조합해 엔더 월드에 가세요.\n엔더드래곤을 잡은 사람은", "매우 좋은 일이 있을거에요!\n어, 인첸트 주민으로부터 업그레이드를 \n구매해서 더 강해질 수 있어요!\n그러면 희망을 빕니다!");
                             book.setItemMeta(bm);
 
                             plr.getInventory().addItem(book);
