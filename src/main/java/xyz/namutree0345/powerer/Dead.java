08659181a6425f3885ce0eebeefdcf64888c76ed
TreeKR <johanbag290@gmail.com>
2021-02-28 14:31:24 +0900

    f

--- src/main/java/xyz/namutree0345/powerer/Dead.java
+++ src/main/java/xyz/namutree0345/powerer/Dead.java
@@ -13,10 +13,18 @@ public class Dead implements Listener {
     @EventHandler
     public void onDead(PlayerDeathEvent event) {
         event.getEntity().setGameMode(GameMode.SPECTATOR);
-        event.getEntity().sendTitle(ChatColor.RED + "죽었습니다!", "3초 뒤 리스폰됩니다.", 5, 60, 5);
+        final int[] a = {3};
+        int ai = Bukkit.getScheduler().scheduleSyncRepeatingTask(Powerer.getPlugin(Powerer.class), new Runnable() {
+            @Override
+            public void run() {
+                event.getEntity().sendTitle(ChatColor.RED + "죽었습니다!", a[0] + "초 뒤 리스폰됩니다.", 5, 60, 5);
+                a[0]--;
+            }
+        }, 0L, 20L);
         Bukkit.getScheduler().scheduleSyncDelayedTask(Powerer.getPlugin(Powerer.class), new Runnable() {
                     @Override
                     public void run() {
+                        Bukkit.getScheduler().cancelTask(ai);
                         event.getEntity().teleport(Objects.requireNonNull(event.getEntity().getBedSpawnLocation()));
                         event.getEntity().setGameMode(GameMode.SURVIVAL);
                         event.getEntity().sendTitle(ChatColor.GOLD + "리스폰!", "", 5, 60, 5);
