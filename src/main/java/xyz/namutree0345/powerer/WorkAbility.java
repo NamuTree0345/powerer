b4c5aca825d4a314b36b805a1f77ecbdbac33439
TreeKR <johanbag290@gmail.com>
2021-03-07 16:28:41 +0900

    l

--- src/main/java/xyz/namutree0345/powerer/WorkAbility.java
+++ src/main/java/xyz/namutree0345/powerer/WorkAbility.java
@@ -1,14 +1,14 @@
 package xyz.namutree0345.powerer;
 
-import com.destroystokyo.paper.event.player.PlayerJumpEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 
 public class WorkAbility implements Listener {
 
     @EventHandler
-    public void doubleJump(PlayerJumpEvent event) {
-        if(event.getPlayer().isJumping()) {
+    public void doubleJump(PlayerToggleSneakEvent event) {
+        if(event.getPlayer().isSprinting() && event.getPlayer().isJumping()) {
             event.getPlayer().getLocation().setY(event.getPlayer().getLocation().getY() + 1);
         }
     }
